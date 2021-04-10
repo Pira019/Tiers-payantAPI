1da45e3be7d24afe91c522648289901828caba3d
Pira019 <pires00243@gmail.com>
2021-04-10 10:55:34 +0100

    P -model

--- projects/src/main/java/ania/projects/controller/TestController.java
+++ projects/src/main/java/ania/projects/controller/TestController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ania.projects.model.Medecin;
 import ania.projects.service.MedecinService;
 
 @RestController
+@RequestMapping("test/")
 public class TestController {
 
 	@Autowired
